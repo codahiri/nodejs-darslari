ade46952720e7ae72717f78e126d8414fc6b1653
ilosrim <ilosrim@yahoo.com>
2021-12-06 22:14:55 +0500

    route id search

--- 03-express/express-demo/index.js
+++ 03-express/express-demo/index.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
 
+const books = [{
+    id: 1,
+    name: 'Usta va Margarita'
+  },
+  {
+    id: 2,
+    name: 'Urush va Tinchlik'
+  },
+  {
+    id: 3,
+    name: 'Zardusht tavallosi'
+  }
+];
+
 app.get('/', (req, res) => {
   res.send('Salom Dunyo!');
   res.end();
 });
 
 app.get('/api/books', (req, res) => {
-  res.send(['Usta va Margarita', 'Urush va Tinchlik', 'Zardusht tavallosi'])
+  res.send(books);
 });
 
 app.get('/api/books/:id', (req, res) => {
-  res.send(req.params.id);
+  const book = books.find(b => b.id === parseInt(req.params.id));
+  if (!book) {
+    res.status(404).send(`Berilgan id dagi kitob topilmadi!`)
+  }
+  res.send(book);
 });
 
 app.get('/api/articles/:year/:month', (req, res) => {
